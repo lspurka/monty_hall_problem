630585a447c12384799f4759abd1def641f1c3c2
Lindsay <lindsay@L-NYC-MVERZEL0.local>
2021-01-31 16:26:24 -0500

    minor updates

--- monty_hall_simulation.py
+++ monty_hall_simulation.py
@@ -11,29 +11,24 @@ def run_simulation(generate_plot=True, num_of_minigames=100, num_of_simulations=
     :param generate_plot: bool, if True, will generate plot
     :param num_of_minigames: int, number of minigames within each simulation
     :param num_of_simulations: int, number of simulated minigames
-    :return change_wins_list
+    :return None
     """
 
-    # TODO update these variable names
     no_change_wins_list = []
     change_wins_list = []
 
-    # TODO Double-check logic:
     for y in range(num_of_simulations):
         no_change_wins = 0.0
         change_wins = 0.0
         for x in range(num_of_minigames):
-            random_list = random.sample(['C','G','G'], 3)
+            random_list = random.sample(["C","G","G"], 3)
             random_choice = random.sample(range(3), 1)[0]
             # no change
             if random_list[random_choice] == 'C':
                 no_change_wins += 1
             # change answer
-            other_2_options = []
-            for count, item in zip(range(3), random_list):
-                if count != random_choice:
-                    other_2_options.append(random_list[count])
-            if 'C' in other_2_options:
+            random_list.pop(random_choice)
+            if "C" in random_list:
                 change_wins += 1  
         
         no_change_wins_list.append((no_change_wins / num_of_minigames))
@@ -43,19 +38,19 @@ def run_simulation(generate_plot=True, num_of_minigames=100, num_of_simulations=
     no_change_percent_right = round(100 * (no_change_wins / num_of_minigames), 2)
     change_percent_right = round(100 * (change_wins / num_of_minigames), 2)
 
-    print(f"If you never change your answer in {num_of_minigames} tries, you'll win the car " \
-            "{no_change_percent_right}% of the time.")
+    print(f"If you never change your answer in {num_of_minigames} tries, you'll win the car about " \
+            f"{no_change_percent_right}% of the time.")
 
-    print(f"If you always change your answer in {num_of_minigames} tries, you'll win the car {change_percent_right}% " \
-            "of the time.")
+    print(f"So if you always change your answer in {num_of_minigames} tries, you'll win the car about " \
+            f"{change_percent_right}% of the time.")
 
     if generate_plot:
-        plot_histogram(change_wins_list=change_wins_list, num_of_minigames=num_of_minigames, 
+        plot_histogram(wins_list=change_wins_list, num_of_minigames=num_of_minigames, 
                 num_of_simulations=num_of_simulations)
 
 
-def plot_histogram(change_wins_list, num_of_minigames, num_of_simulations):
-    sns.distplot(change_wins_list)
+def plot_histogram(wins_list, num_of_minigames, num_of_simulations):
+    sns.histplot(wins_list)
     plt.title(f"Frequency of Wins if you always change your mind ({num_of_minigames} games of {num_of_simulations} " \
             "simulations)")
     plt.xlabel("Fraction of Wins in One Game")
@@ -66,11 +61,12 @@ def plot_histogram(change_wins_list, num_of_minigames, num_of_simulations):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Monty Hall Simulation!")
-    parser.add_argument("--plot", "-p", type=bool, dest="generate_plot", help="bool, if True, plot will be generated.")
+    parser.add_argument("--plot", "-p", type=bool, dest="generate_plot", help="bool, if True, plot will be generated."  
+            , default=True)
     parser.add_argument("--num_of_minigames", "-m", type=int, dest="num_of_minigames", help="Number of minigames " \
-            "within each simulation, 100 as default.")
+            "within each simulation, 100 as default.", default=100)
     parser.add_argument("--num_of_simulations", "-s", type=int, dest="num_of_simulations", help="Number of simulated " \
-            "minigames, 1000 as default.")
+            "minigames, 1000 as default.", default=1000)
     args = parser.parse_args()
     generate_plot, num_of_minigames, num_of_simulations = args.generate_plot, args.num_of_minigames, args.num_of_simulations
     run_simulation(generate_plot=generate_plot, num_of_minigames=num_of_minigames, num_of_simulations=num_of_simulations)
